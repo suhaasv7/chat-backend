157378e39759490f7cb9212becb93b62f082ec95
suhaasvijay <suhaasvijay7@gmail.com>
2024-01-23 22:24:46 +0530

    made design change and added time for the messages

--- js/client.js
+++ js/client.js
@@ -1,40 +1,124 @@
 const socket = io("https://chat-application-skoy.onrender.com");
 
+var audio = new Audio("public/notification.mp3");
+
+// const elementsValues = {
+//   send: "",
+//   msgInput: "",
+//   name: "",
+//   mainContent: "",
+//   myModal: "",
+//   submitBtn: "",
+//   userName: "",
+// };
+
 const form = document.getElementById("send");
 const messageInp = document.getElementById("mesInput");
+const displayName = document.getElementById("name");
 const messageContainer = document.querySelector(".container");
-var audio = new Audio('public/notification.mp3');
+const activeMembers = document.getElementById("activeMembers");
+let mainContent = document.getElementById("mainContent");
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("submitBtn");
+let input = document.getElementById("userName");
+
+// Object.keys(elementsValues).forEach((element) => {
+//   elementsValues[element] = document.getElementById(element);
+// });
+
+let members = [];
+
+const appendActiveMember = () => {
+  members.forEach((member) => {
+    const activeMember = document.createElement("div");
+    activeMember.innerText = member;
+    activeMember.classList.add("activeMember");
+    activeMembers.append(activeMember);
+  });
+};
 
 const append = (message, position) => {
-  const messageElement = document.createElement('div');
-  messageElement.innerText = message;
-  messageElement.classList.add('message');
-  messageElement.classList.add(position);
-  messageContainer.append(messageElement);
-  if (position == 'left') {
+  const singleMsgDiv = document.createElement("div");
+  singleMsgDiv.innerHTML = `
+    <div class="message ${position}">
+    ${message} <span class="timestamp-span">${new Date().toLocaleTimeString()}</span
+    ></div> `;
+  messageContainer.append(singleMsgDiv);
+  if (position == "left") {
     audio.play();
   }
-}
+  messageContainer.scrollTop = messageContainer.scrollHeight;
+};
 
-const name = prompt("Enter your name to join");
-socket.emit("new-user-joined", name);
+const appendAlert = (message, position) => {
+  const messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  messageElement.classList.add("alert");
+  messageElement.classList.add(position);
+  messageContainer.append(messageElement);
+  audio.play();
+  messageContainer.scrollTop = messageContainer.scrollHeight;
+};
 
-form.addEventListener("submit", (e) => {
+window.onload = function () {
+  modal.style.display = "block";
+};
+
+btn.onclick = function () {
+  const userName = input.value.trim();
+  if (userName !== "") {
+    socket.emit("new-user-joined", userName);
+    modal.style.display = "none";
+    mainContent.style.display = "block";
+    displayName.innerText = userName;
+  } else {
+    alert("Name cannot be empty. Please enter your name to join");
+  }
+};
+
+form?.addEventListener("submit", (e) => {
   e.preventDefault();
   const message = messageInp.value;
-  append(`You: ${message}`, 'right');
-  socket.emit("send", message);
-  messageInp.value = ''
-})
+  if (message.trim() !== "") {
+    append(`You: ${message}`, "right");
+    socket.emit("send", message);
+    messageInp.value = "";
+  } else {
+    alert("Please enter a message before submitting.");
+  }
+});
 
-socket.on('user-joined', name => {
-  append(`${name} joined the chat`, 'right')
-})
+socket.on("user-joined", (name) => {
+  if (name !== null) {
+    appendAlert(`${name} joined the chat`, "left");
+    members.push(name);
+    console.log(
+      "%c 🍠 members: ",
+      "font-size:12px;background-color: #EA7E5C;color:#fff;",
+      members
+    );
+    // const activeMember = document.createElement("div");
+    // activeMember.innerText = name;
+    // activeMembers.append(activeMember);
+    appendActiveMember();
+  }
+});
 
-socket.on('receive', data => {
-  append(`${data.name} : ${data.message}`, 'left')
-})
+socket.on("receive", (data) => {
+  if (data.name !== null) {
+    append(`${data.name} : ${data.message}`, "left");
+  }
+});
 
-socket.on('left', name => {
-  append(`${name} left the chat`, 'left')
-})
+socket.on("left", (name) => {
+  if (name !== null) {
+    appendAlert(`${name} left the chat`, "left");
+    members = members.filter((member) => member !== name);
+    console.log(
+      "%c 🥨 members: ",
+      "font-size:12px;background-color: #B03734;color:#fff;",
+      members
+    );
+    appendActiveMember();
+  }
+});
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,36 +1,41 @@
 //node server which will handle socket io connections
-const express = require("express")
+const express = require("express");
 var app = express();
 var server = app.listen(8000);
-var io = require('socket.io')(server, {
+var io = require("socket.io")(server, {
   cors: {
-    origin: '*',
-  }
+    origin: "*",
+  },
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 //creating a healthCheck path to check if the server is running
-app.get('/healthCheck', (req, res) => {
-  res.send('Server is running');
+app.get("/healthCheck", (req, res) => {
+  res.send("Server is running");
 });
 
 const users = {};
 
 io.on("connection", (socket) => {
-  socket.on('new-user-joined', name => {
-    users[socket.id] = name;
-    socket.broadcast.emit('user-joined', name);
+  socket.on("new-user-joined", (name) => {
+    if (name) {
+      users[socket.id] = name;
+      socket.broadcast.emit("user-joined", name);
+    }
   });
 
-  socket.on('send', message => {
-    socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", {
+      message: message,
+      name: users[socket.id],
+    });
   });
 
-  socket.on('disconnect', message => {
-    socket.broadcast.emit('left', users[socket.id]);
+  socket.on("disconnect", (message) => {
+    socket.broadcast.emit("left", users[socket.id]);
     delete users[socket.id];
   });
-})
+});
