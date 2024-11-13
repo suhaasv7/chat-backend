f6f01228c5403b0b0a8912acdcb4a3203d1121c2
Suhaas V <suhaasvijay7@gmail.com>
2024-11-13 23:02:11 +0530

    Converting to typescript

---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import express, { Request, Response } from "express";
+import { Server as HttpServer } from "http";
+import { Server, Socket } from "socket.io";
+
+// Interface for users object
+interface Users {
+  [key: string]: string;
+}
+
+// Interface for the message object
+interface MessageData {
+  message: string;
+  name: string;
+}
+
+const app = express();
+const server: HttpServer = app.listen(8000);
+const io: Server = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+app.get("/", (req: Request, res: Response): void => {
+  res.send("Hello World!");
+});
+
+app.get("/healthCheck", (req: Request, res: Response): void => {
+  res.send("Server is running");
+});
+
+const users: Users = {};
+
+io.on("connection", (socket: Socket): void => {
+  socket.on("new-user-joined", (name: string): void => {
+    if (name) {
+      users[socket.id] = name;
+      socket.broadcast.emit("user-joined", name);
+    }
+  });
+
+  socket.on("send", (message: string): void => {
+    socket.broadcast.emit("receive", {
+      message: message,
+      name: users[socket.id],
+    } as MessageData);
+  });
+
+  socket.on("disconnect", (): void => {
+    socket.broadcast.emit("left", users[socket.id]);
+    delete users[socket.id];
+  });
+});
